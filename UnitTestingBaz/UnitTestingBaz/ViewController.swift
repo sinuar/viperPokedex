6f01b76dc197ef1523fede9afe8bde9e1fb89dd4
Heber Raziel Alvarez Ruedas <heber.alvarez@elektra.com.mx>
2022-10-31 09:50:37 -0600

    Add blank project

---
+++ UnitTestingBaz/UnitTestingBaz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UnitTestingBaz
+//
+//  Created by Heber Raziel Alvarez Ruedas on 31/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
